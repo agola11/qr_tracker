0a7f0c96d83c2c0ca86360b2001d6385714c4db1
David Fridovich-Keil <davidfridovichkeil@nat-oitwireless-inside-vapornet100-b-5582.Princeton.EDU>
2014-12-14 13:17:40 -0500

    changed test orb code

--- qrTrackerPython/test_orb_matching.py
+++ qrTrackerPython/test_orb_matching.py
@@ -12,32 +12,36 @@ import drawMatches
 
 import time, sys
 
-IMGPATH = "./"
-QRFILENAME = "example.png"
-BGFILENAME = "nassau.jpg"
+IMGPATH = "../images/"
+QRFILENAME = "qr_example.png"
+ARFILENAME = "ar_example.png"
+TESTFILENAME = "test1.jpg"
 
 # read images
-bg = cv2.imread(IMGPATH + BGFILENAME)
-bg = cv2.resize(bg, (int(round(bg.shape[1]*0.25)), 
-                     int(round(bg.shape[0]*0.25))))
+test = cv2.imread(IMGPATH + TESTFILENAME)
+#test = cv2.resize(test, (int(round(test.shape[1]*0.25)), 
+#                     int(round(test.shape[0]*0.25))))
 
 qr = cv2.imread(IMGPATH + QRFILENAME)
 qr = cv2.resize(qr, (300, 300))
 
+ar = cv2.imread(IMGPATH + ARFILENAME)
+ar = cv2.resize(ar, (300, 300))
+
 # create random composite
-random.seed()
-scale = random.uniform(0.25, 2.0)
-scaled_qr = cv2.resize(qr, (int(round(scale*qr.shape[1])), 
-                            int(round(scale*qr.shape[0]))))
-paste_loc = (random.randint(0, bg.shape[1] - scaled_qr.shape[1] - 1), 
-             random.randint(0, bg.shape[0] - scaled_qr.shape[0] - 1))
-im = bg
-im[paste_loc[1]:(paste_loc[1] + scaled_qr.shape[1]), 
-   paste_loc[0]:(paste_loc[0] + scaled_qr.shape[0])] = scaled_qr
+#random.seed()
+#scale = random.uniform(0.25, 2.0)
+#scaled_qr = cv2.resize(qr, (int(round(scale*qr.shape[1])), 
+#                            int(round(scale*qr.shape[0]))))
+#paste_loc = (random.randint(0, test.shape[1] - scaled_qr.shape[1] - 1), 
+#             random.randint(0, test.shape[0] - scaled_qr.shape[0] - 1))
+#im = test
+#im[paste_loc[1]:(paste_loc[1] + scaled_qr.shape[1]), 
+#   paste_loc[0]:(paste_loc[0] + scaled_qr.shape[0])] = scaled_qr
 
 # convert to grayscale
 gray_qr = cv2.cvtColor(qr, cv2.COLOR_RGB2GRAY)
-gray_im = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+gray_test = cv2.cvtColor(test, cv2.COLOR_RGB2GRAY)
 
 # start timer
 starttime = time.time()
@@ -45,40 +49,40 @@ starttime = time.time()
 # find ORB keypoints
 orb = cv2.ORB()
 kp_qr, des_qr = orb.detectAndCompute(gray_qr, None)
-kp_im, des_im = orb.detectAndCompute(gray_im, None)
+kp_test, des_test = orb.detectAndCompute(gray_test, None)
 
 # do feature matching
 bf = cv2.BFMatcher(normType = cv2.NORM_HAMMING)
-matches = bf.knnMatch(des_qr, des_im, k=2)
+matches_qr = bf.knnMatch(des_qr, des_test, k=2)
 
 # ratio test
-good_matches = []
-for m,n in matches:
+good_matches_qr = []
+for m,n in matches_qr:
     if m.distance < 0.75*n.distance:
-        good_matches.append(m)
+        good_matches_qr.append(m)
 
 # halt if not enough good matches
-if len(good_matches) < 4:
+if len(good_matches_qr) < 4:
     print "Not enough good matches to estimate a homography."
     sys.exit()
 
 # estimate homography
 pts_qr = np.float32([kp_qr[m.queryIdx].pt 
-                     for m in good_matches]).reshape(-1,1,2)
-pts_im = np.float32([kp_im[m.trainIdx].pt 
-                     for m in good_matches]).reshape(-1,1,2)
-H, mask = cv2.findHomography(pts_qr, pts_im, cv2.RANSAC, 5.0)
+                     for m in good_matches_qr]).reshape(-1,1,2)
+pts_test = np.float32([kp_test[m.trainIdx].pt 
+                     for m in good_matches_qr]).reshape(-1,1,2)
+H_qr, mask_qr = cv2.findHomography(pts_qr, pts_test, cv2.RANSAC, 5.0)
 
 # draw boundary of qr code
 qr_h, qr_w = gray_qr.shape
 corners_qr = np.float32([[0, 0], [0, qr_h-1], 
                          [qr_w-1, qr_h-1], [qr_w-1, 0]]).reshape(-1,1,2)
-corners_im = cv2.perspectiveTransform(corners_qr, H)
-cv2.polylines(gray_im, [np.int32(corners_im)], True, 0, 5)
-gray_im.shape
+corners_test_qr = cv2.perspectiveTransform(corners_qr, H_qr)
+cv2.polylines(gray_test, [np.int32(corners_test_qr)], True, 0, 5)
+gray_test.shape
 
 # filter out inliers
-matchesMask = mask.ravel().tolist()
+matchesMask_qr = mask_qr.ravel().tolist()
 inliers = []
 for i, m in enumerate(matchesMask):
     if m == 1:
@@ -88,4 +92,4 @@ for i, m in enumerate(matchesMask):
 print "Total elapsed time: " + str(time.time() - starttime) + " seconds"
 
 # show images
-drawMatches.drawMatches(gray_qr, kp_qr, gray_im, kp_im, inliers) 
+drawMatches.drawMatches(gray_qr, kp_qr, gray_test, kp_test, inliers) 
