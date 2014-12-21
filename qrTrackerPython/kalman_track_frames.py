a41522d6619f51fc8de3ea8b334542c4cc2622fd
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2014-12-20 20:06:29 -0500

    started kalman tracking

--- qrTrackerPython/kalman_track_frames.py
+++ qrTrackerPython/kalman_track_frames.py
@@ -2,122 +2,124 @@
 Course: COS 429
 Author: David Fridovich-Keil
 
-Test feature matching using ORB features and RANSAC.
+Use Kalman filter to guide matching on the next frame, given the matching
+result from the previous frame.
 """
 
 import numpy as np
 import random, math
 import cv2
 import drawMatches
+import pykalman
 
-import time, sys
+import time, sys, os
 
 # file paths
 IMGPATH = "../images/"
-TEMPFILENAME = "orange_chinese.JPG"
-TESTFILENAME = "orange_chinese_4.JPG"
+EXFILENAME = "orange_chinese.JPG"
+VIDEOPATH = "../videos/orange_chinese/"
+VIDEOBASENAME = "orange_chinese%4d.jpg"
 
-# read images
-test = cv2.imread(IMGPATH + TESTFILENAME)
-test = cv2.resize(test, (int(round(test.shape[1]*0.25)), 
-                     int(round(test.shape[0]*0.25))))
-#test = test[100:600, 50:400, :] # for orange_chinese_2.JPG
-#test = test[200:300, 200:400, :] # for orange_chinese_3.JPG
+# initialize kalman parameters
+"""
+ADD CODE HERE.
+"""
 
-temp = cv2.imread(IMGPATH + TEMPFILENAME)
-temp = cv2.resize(temp, (int(round(temp.shape[1]*0.25)), 
-                     int(round(temp.shape[0]*0.25))))
-
-# create random composite
-#random.seed()
-#scale = random.uniform(0.25, 2.0)
-#scaled_temp = cv2.resize(temp, (int(round(scale*temp.shape[1])), 
-#                            int(round(scale*temp.shape[0]))))
-#paste_loc = (random.randint(0, test.shape[1] - scaled_temp.shape[1] - 1), 
-#             random.randint(0, test.shape[0] - scaled_temp.shape[0] - 1))
-#im = test
-#im[paste_loc[1]:(paste_loc[1] + scaled_temp.shape[1]), 
-#   paste_loc[0]:(paste_loc[0] + scaled_temp.shape[0])] = scaled_temp
-
-# convert to grayscale
-hsv_temp = cv2.cvtColor(temp, cv2.COLOR_RGB2HSV)
-hue_temp = hsv_temp[:,:,0]
-gray_temp = cv2.cvtColor(temp, cv2.COLOR_RGB2GRAY)
-
-
-hsv_test = cv2.cvtColor(test, cv2.COLOR_RGB2HSV)
-hue_test = hsv_test[:,:,0]
-gray_test = cv2.cvtColor(test, cv2.COLOR_RGB2GRAY)
-
-# filter colors
+# initialize color filter parameters
 UPPERBOUND_ORANGE = 25
 LOWERBOUND_ORANGE = 110
 LOWERBOUND_LUM = 140
 MEDIANSIZE = 3
 
-indices_temp = np.logical_or(np.logical_and(hue_temp > UPPERBOUND_ORANGE, 
-                                       hue_temp < LOWERBOUND_ORANGE),
-                             gray_temp > LOWERBOUND_LUM)
-gray_temp[indices_temp] = 255
-gray_temp[np.logical_not(indices_temp)] = 0
+# initialize other recurrent parameters
+last_topleft = (None, None)
+last_botright = (None, None)
+CROPFACTOR = 1.5
+frame = 1
 
-indices_test = np.logical_or(np.logical_and(hue_test > UPPERBOUND_ORANGE, 
-                                       hue_test < LOWERBOUND_ORANGE),
-                             gray_test > LOWERBOUND_LUM)
-gray_test[indices_test] = 255
-gray_test[np.logical_not(indices_test)] = 0
+# main loop
+while os.path.isfile(VIDEOPATH + VIDEOBASENAME % frame):
 
-gray_temp = cv2.medianBlur(gray_temp, MEDIANSIZE)
-gray_test = cv2.medianBlur(gray_test, MEDIANSIZE)
-
-# start timer
-starttime = time.time()
-
-# find ORB keypoints
-orb = cv2.ORB()
-kp_temp, des_temp = orb.detectAndCompute(gray_temp, None)
-kp_test, des_test = orb.detectAndCompute(gray_test, None)
-
-# do feature matching
-bf = cv2.BFMatcher(normType = cv2.NORM_HAMMING)
-matches_temp = bf.knnMatch(des_temp, des_test, k=2)
-
-# ratio test
-good_matches_temp = []
-for m,n in matches_temp:
-    if m.distance < 0.8*n.distance:
-        good_matches_temp.append(m)
+    # read images
+    test = cv2.imread(VIDEOPATH + VIDEOBASENAME % frame)
+    ex = cv2.imread(IMGPATH + EXFILENAME)
+    
+    # convert to grayscale
+    hsv_ex = cv2.cvtColor(ex, cv2.COLOR_RGB2HSV)
+    hue_ex = hsv_ex[:,:,0]
+    gray_ex = cv2.cvtColor(ex, cv2.COLOR_RGB2GRAY)
 
 
-# halt if not enough good matches
-if len(good_matches_temp) < 4:
-    print "Not enough good matches to estimate a homography."
-    sys.exit()
+    hsv_test = cv2.cvtColor(test, cv2.COLOR_RGB2HSV)
+    hue_test = hsv_test[:,:,0]
+    gray_test = cv2.cvtColor(test, cv2.COLOR_RGB2GRAY)
 
-# estimate homography
-pts_temp = np.float32([kp_temp[m.queryIdx].pt 
-                     for m in good_matches_temp]).reshape(-1,1,2)
-pts_test = np.float32([kp_test[m.trainIdx].pt 
-                     for m in good_matches_temp]).reshape(-1,1,2)
-H_temp, mask_temp = cv2.findHomography(pts_temp, pts_test, cv2.RANSAC, 5.0)
+    # filter colors
+    indices_ex = np.logical_or(np.logical_and(hue_ex > UPPERBOUND_ORANGE, 
+                                              hue_ex < LOWERBOUND_ORANGE),
+                               gray_ex > LOWERBOUND_LUM)
+    gray_ex[indices_ex] = 255
+    gray_ex[np.logical_not(indices_ex)] = 0
 
-# draw boundary of temp code
-temp_h, temp_w = gray_temp.shape
-corners_temp = np.float32([[0, 0], [0, temp_h-1], 
-                         [temp_w-1, temp_h-1], [temp_w-1, 0]]).reshape(-1,1,2)
-corners_test_temp = cv2.perspectiveTransform(corners_temp, H_temp)
-cv2.polylines(gray_test, [np.int32(corners_test_temp)], True, 120, 5)
-gray_test.shape
+    indices_test = np.logical_or(np.logical_and(hue_test > UPPERBOUND_ORANGE, 
+                                                hue_test < LOWERBOUND_ORANGE),
+                                 gray_test > LOWERBOUND_LUM)
+    gray_test[indices_test] = 255
+    gray_test[np.logical_not(indices_test)] = 0
 
-# filter out inliers
-matchesMask_temp = mask_temp.ravel().tolist()
-inliers = []
-for i, m in enumerate(matchesMask_temp):
-    if m == 1:
-        inliers.append(good_matches_temp[i])
+    gray_ex = cv2.medianBlur(gray_ex, MEDIANSIZE)
+    gray_test = cv2.medianBlur(gray_test, MEDIANSIZE)
 
-# print elapsed time
-print "Total elapsed time: " + str(time.time() - starttime) + " seconds"
+    # start timer
+    starttime = time.time()
 
-# show images
-drawMatches.drawMatches(gray_temp, kp_temp, gray_test, kp_test, inliers) 
+    # find ORB keypoints
+    orb = cv2.ORB()
+    kp_ex, des_ex = orb.detectAndCompute(gray_ex, None)
+    kp_test, des_test = orb.detectAndCompute(gray_test, None)
+
+    # do feature matching
+    bf = cv2.BFMatcher(normType = cv2.NORM_HAMMING)
+    matches_ex = bf.knnMatch(des_ex, des_test, k=2)
+
+    # ratio test
+    good_matches_ex = []
+    for m,n in matches_ex:
+        if m.distance < 0.8*n.distance:
+            good_matches_ex.append(m)
+
+
+    # halt if not enough good matches
+    if len(good_matches_ex) < 4:
+        print "Not enough good matches to estimate a homography."
+        sys.exit()
+
+    # estimate homography
+    pts_ex = np.float32([kp_ex[m.queryIdx].pt 
+                         for m in good_matches_ex]).reshape(-1,1,2)
+    pts_test = np.float32([kp_test[m.trainIdx].pt 
+                           for m in good_matches_ex]).reshape(-1,1,2)
+    H_ex, mask_ex = cv2.findHomography(pts_ex, pts_test, cv2.RANSAC, 5.0)
+
+    # draw boundary of ex code
+    ex_h, ex_w = gray_ex.shape
+    corners_ex = np.float32([[0, 0], [0, ex_h-1], 
+                             [ex_w-1, ex_h-1], [ex_w-1, 0]]).reshape(-1,1,2)
+    corners_test_ex = cv2.perspectiveTransform(corners_ex, H_ex)
+    cv2.polylines(gray_test, [np.int32(corners_test_ex)], True, 120, 5)
+    gray_test.shape
+
+    # filter out inliers
+    matchesMask_ex = mask_ex.ravel().tolist()
+    inliers = []
+    for i, m in enumerate(matchesMask_ex):
+        if m == 1:
+            inliers.append(good_matches_ex[i])
+
+    # print elapsed time
+    print "Total elapsed time for frame " + frame + ": " + \
+        str(time.time() - starttime) + " seconds"
+    frame = frame + 1
+
+    # show images
+    drawMatches.drawMatches(gray_ex, kp_ex, gray_test, kp_test, inliers) 
